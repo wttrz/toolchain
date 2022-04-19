07dfbcf45018378fffa0c9fbd3050cd9b44910fd
will <wilfried.nguessan@outlook.com>
2022-04-19 21:35:06 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="serene",
+    version="v0.0.1",
+    description="common seo operations",
+    author="wttrz",
+    install_requires=required,
+    packages=find_packages(),
+    entry_points={"console_scripts": ["serene=src.app:main"]},
+)
