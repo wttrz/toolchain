8414274dc596c3dd1419c3bee707dc47b203c2bd
will <wilfried.nguessan@outlook.com>
2022-05-16 12:50:07 +0200

    add uninmplemented moa

---
+++ src/moa.py
@@ -0,0 +1,9 @@
+# do nothing script for the moa
+# allow intermediate steps storing
+# store outputs in a desktop with unique id
+# prompt user for responses
+# allow resuming or starting anew
+# allow canceling
+
+# https://blog.danslimmon.com/2019/07/15/do-nothing-scripting-the-key-to-gradual-automation/
+# https://news.ycombinator.com/item?id=29083367
