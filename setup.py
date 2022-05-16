a229bfa83159be22b73500eabbd131a733d58a28
will <wilfried.nguessan@outlook.com>
2022-05-16 12:44:56 +0200

    update semver

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+from src.cli import semver
+
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
+
 setup(
     name="serene",
-    version="v0.0.3",
+    version=semver,
     description="common seo operations",
     author="wttrz",
     install_requires=required,
