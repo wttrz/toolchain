26dee4e01ce41eb46fc00cb01c193056e4e1063b
will <wilfried.nguessan@outlook.com>
2022-05-16 12:45:56 +0200

    add formatting to calls

--- src/authentication.py
+++ src/authentication.py
@@ -1,18 +1,37 @@
+"""
+Perform authentication operations.
+
+This module contains routines to authenticate in various API.
+
+References:
+
+> https://google-auth.readthedocs.io/en/master/
+> https://developers.google.com/identity/protocols/oauth2/scopes
+> https://precisdigital.atlassian.net/wiki/spaces/TECH/pages/1335132191/GCP+Authentication+Guidelines
+> https://docs.google.com/presentation/d/1AQTVogaSoh3ZKq4LrQbgonzg7L-MCQ55BZ7xu255zdg/edit?usp=sharing
+"""
+
 import os
 import sys
+from typing import Optional
+
 import requests
 
+from src.formatting import fprint
 
-def get_api_key(api_name):
+
+def get_api_key(api_name: str) -> Optional[str]:
     if api_name not in ["semrush", "valueserp"]:
-        sys.exit(f"[error] {api_name} is not supported")
+        fprint("error", f"{api_name} is not supported")
+        sys.exit()
     if api_name == "semrush":
         return os.environ.get("SEMRUSHKEY")
     if api_name == "valueserp":
         return os.environ.get("VALUESERPKEY")
+    return None
 
 
-def get_api_credit(api_name):
+def get_api_credit(api_name: str) -> int:
     apikey = get_api_key(api_name)
     if api_name == "semrush":
         endpoint = "http://www.semrush.com/users/countapiunits.html"
@@ -26,7 +45,8 @@ def get_api_credit(api_name):
         r_dict = r.json()
         credit = r_dict["account_info"]["monthly_credits_remaining"]
     if credit < 1000:
-        sys.exit(f"credit limit reached, get in touch to get more credits")
+        fprint("error", "credt limit reached, contact seo team for more credits")
+        sys.exit()
     else:
-        print(f"{credit} credit points left for the {api_name} api")
+        fprint("info", f"{credit} credit points left for the {api_name} api")
     return credit
