40cc2f904ea1d4393142237cc9b0a3d42ee7cdcc
will <wilfried.nguessan@outlook.com>
2022-05-16 12:51:07 +0200

    refactor the module

--- src/kwlist.py
+++ src/kwlist.py
@@ -1,84 +1,60 @@
 """
-Construct a keyword set based on SERPs suggestions.
+Construct a keyword set based on Google SERPs suggestions.
 
 Given a list of keywords, this algorithm recursively fetches related questions and searches until a cutoff point is met.
+
+The cutoff point represents the minimum number of rows to export.
+The output might contain more keywords than the cutoff point. That's because the algorithm collects all keywords on file.
 """
 
-import sys
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from typing import Any, Dict, List
 
 import pandas as pd
 import requests
 
-from src.authentication import get_api_credit, get_api_key
+from src.apicalls import query_valueserp
+from src.formatting import fprint
 
 session = requests.Session()
 
-# TODO: use the get_valueserp queries intead of using build_valueserp_calls
 
-
-def build_valueserp_calls(keywords, location):
-    apikey = get_api_key("valueserp")
-    get_api_credit("valueserp")
-    supported_locations = {
-        "Norway": ["google.no", "no", "no"],
-        "Sweden": ["google.se", "se", "sv"],
-        "Canada": ["google.ca", "ca", "en"],
-        "Denmark": ["google.dk", "dk", "da"],
-        "Finland": ["google.fi", "fi", "fi"],
-        "United States": ["google.com", "us", "en"],
-        "United Kingdom": ["google.co.uk", "uk", "en"],
-    }
-    if location.title() not in supported_locations:
-        sys.exit("[error] unsupported location")
-    locale = supported_locations.get(location.title())
-    google_domain = locale[0]
-    gl = locale[1]
-    hl = locale[2]
-    url = f"https://api.valueserp.com/search?api_key={apikey}"
-    geo = f"&gl={gl}" + f"&location={location}"
-    lang = f"&hl={hl}"
-    domain = f"&google_domain={google_domain}"
-    datafmt = "&output=json&flatten_results=true"
-    api_calls = [url + f"&q={k}" + lang + geo + domain + datafmt for k in keywords]
-    return api_calls
-
-
-# TODO: check how i resolved this in the on page module
-def collect_suggestions(response):
+def collect_related(response: Dict[str, Any]) -> List[str]:
     related = list()
     if "related_searches" in response:
-        searches = [r["query"] for r in response["related_searches"]]
-        related.extend(searches)
+        related.append([i["query"] for i in response["related_searches"]])
     if "related_questions" in response:
-        questions = [r["question"] for r in response["related_questions"]]
-        related.extend(questions)
-    return related
+        related.append([i["question"] for i in response["related_questions"]])
+    return [i for sublist in related for i in sublist]
 
 
-def get_kwlist(keywords, location, cutoff, fpath):
+def get_kwlist(keywords: List[str], location: str, cutoff: int, fpath: Path, cut: List[str]) -> pd.DataFrame:
     total = len(keywords)
-    print(f"[info] collecting keyword list from {total} keywords in {location.title()}.")
-    api_calls = build_valueserp_calls(keywords, location)
-    kw_stack = list()
+    location = location.title()
+    fprint("info", f"collecting keyword list from {total} keywords - location: {location.title()}")
+    kw_stack: List[str] = list()
     kw_stack_length = len(kw_stack)
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        futures = executor.map(session.get, api_calls)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = executor.map(lambda i: query_valueserp(i, location), keywords)
         for f in futures:
             response = f.json()
-            serps = collect_suggestions(response)
-            kw_stack.extend(serps)
-            kw_stack_length += len(serps)
+            related = collect_related(response)
+            kw_stack.extend(related)
+            kw_stack_length += len(related)
     while kw_stack_length < cutoff:
-        stack_calls = build_valueserp_calls(kw_stack, location)
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            futures = executor.map(session.get, stack_calls)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = executor.map(lambda i: query_valueserp(i, location), kw_stack)
             for f in futures:
                 stack_response = f.json()
-                stack_serps = collect_suggestions(stack_response)
-                kw_stack.extend(stack_serps)
-                kw_stack_length += len(stack_serps)
+                stack_related = collect_related(stack_response)
+                kw_stack.extend(stack_related)
+                kw_stack_length += len(stack_related)
     collection = list(map(str.lower, kw_stack))
     dataset = pd.DataFrame(collection, columns=["kwset"])
+    if cut:
+        patterns = "|".join([i for i in cut])
+        dataset = dataset.loc[~dataset["kwset"].str.contains(patterns)]
     dataset.to_csv(fpath)
+    fprint("info", f"keyword list completed ~ find your output @ {fpath}")
     return dataset
